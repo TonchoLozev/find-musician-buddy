f5a4298437cc62ebfc54b4ac0242c9c0e186a41f
Toncho <toncho.lozev@yahoo.com>
2020-04-05 16:01:15 +0300

    add next configured with sass

---
+++ front-end-web/pages/_app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+import App, { Container } from 'next/app';
+import '../itscss/index.scss';
+
+class FindMusicianBuddy extends App
+{
+      render() {
+        const { Component, pageProps } = this.props;
+        
+        return (
+            <>
+                <Head>
+                    <title>Find-Musician-Buddy</title>
+                    <link rel = "shortcut icon" href = "/images/music-player.png"/>
+                </Head>
+                <Component {...pageProps}/>
+                {/* <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+            </>
+        );
+    }
+}
+
+export default FindMusicianBuddy;
---
+++ front-end-web/pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+
+const Home = () => (
+ <div>Home</div>
+)
+
+export default Home
