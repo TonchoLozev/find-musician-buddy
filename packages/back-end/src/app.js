9cfdae92386c1e918cae85b25b026796e4b5f393
Toncho Lozev <toncho.lozev@yahoo.com>
2020-04-09 23:15:38 +0300

    connected back end and front end

--- packages/back-end/src/app.js
+++ packages/back-end/src/app.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ foo: 'bar' }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+
+app.get('/api/foo', (req, res) => {
+    res.json({ foo: 'bar' });
+});
 
 module.exports = app;
