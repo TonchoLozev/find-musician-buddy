9b8f12bf0afa18ba50d5f260758b15aeb6c52172
Toncho <toncho.lozev@yahoo.com>
2020-04-09 17:56:58 +0300

    add elinst and tests to back end solution

--- packages/back-end/src/index.js
+++ packages/back-end/src/index.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import app from './app';
 
 const port = process.env.PORT || 5000;
-const app = express();
 
 app.listen(port, (err) => {
-  if (err) {
-    console.error(`ERROR: ${err.message}`);
-  } else {
-    console.log(`Listening on port ${port}`);
-    console.log();
-  }
+    if (err) {
+        console.error(`ERROR: ${err.message}`);
+    } else {
+        console.log(`Listening on port ${port}`);
+    }
 });
